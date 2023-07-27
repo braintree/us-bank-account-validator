a0ffbf0ed8b64a5281b419035b080b3d3e5acc4e
Philip Hickey <phlickey@users.noreply.github.com>
2023-07-27 18:17:05 +0100

    feat: add checkdigit validation for aba routing numbers

--- src/routing-number.ts
+++ src/routing-number.ts
@@ -16,10 +16,37 @@ export default function (value: string | unknown): BankValidity {
   }
 
   const isValid = value in allowedRoutingNumbers;
-  const isPotentiallyValid = /^\d{0,8}$/.test(value);
+  const isPotentiallyValid = isCheckDigitValid(value);
 
   return {
     isValid,
     isPotentiallyValid: isValid || isPotentiallyValid,
   };
 }
+
+/**
+ * reference: https://en.wikipedia.org/wiki/ABA_routing_transit_number#Check_digit
+ * @param abaRoutingNumber an ABA routing number to be checked
+ * @returns whether check digit of the ABA routing number is valid
+ */
+const isCheckDigitValid = (abaRoutingNumber: string) => {
+  try {
+    if (!/^\d+$/.test(abaRoutingNumber)) return false;
+    if (abaRoutingNumber.length !== 9) return false;
+
+    const d = abaRoutingNumber.split("").map((digit) => parseInt(digit));
+
+    const res =
+      // prettier-ignore
+      (
+        (3 * (d[0] + d[3] + d[6])) +
+        (7 * (d[1] + d[4] + d[7])) +
+        (1 * (d[2] + d[5] + d[8]))
+      )
+            % 10 === 0;
+
+    return res;
+  } catch (e) {
+    return false;
+  }
+};
